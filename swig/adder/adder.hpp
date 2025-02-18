32a5ad256165c0a3e7594a94df6853d97efe5441
Sam Title <samstitle@gmail.com>
2025-02-18 05:59:52 +0000

    Formatting

--- swig/adder/adder.hpp
+++ swig/adder/adder.hpp
@@ -1,14 +1,14 @@
 #ifndef _ADDER_H_
 #define _ADDER_H_
 
-class Adder
-{
-public:
-  Adder(): _v(0) {};
+class Adder {
+ public:
+  Adder() : _v(0){};
   void Add(int v);
   int Get();
-private:
+
+ private:
   int _v;
 };
 
-#endif // _ADDER_H_
+#endif  // _ADDER_H_
